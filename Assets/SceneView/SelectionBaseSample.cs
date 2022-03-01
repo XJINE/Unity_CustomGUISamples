02e7ea63ce4f14a8ff91cc74dd7ebe3eb97f3d72
XJINE <xjine.andorelseif@gmail.com>
2022-03-01 11:42:08 +0900

    Add SceneViewSample.

---
+++ Assets/SceneView/SelectionBaseSample.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+// NOTE:
+// In the Scene View, when clicking to object,
+// Unity will select the root of the object (means SelectionBase).
+
+[SelectionBase]
+public class SelectionBaseSample : MonoBehaviour { }
